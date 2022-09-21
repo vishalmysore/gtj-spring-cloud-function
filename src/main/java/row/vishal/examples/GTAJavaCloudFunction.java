a7770e08234e2d89ca0afc88119efe932bb46fc1
Vishal Row Mysore <visrow@gmail.com>
2022-09-21 17:15:30 -0400

    first commit

---
+++ src/main/java/row/vishal/examples/GTAJavaCloudFunction.java
@@ -0,0 +1,26 @@
+package row.vishal.examples;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+//@SpringBootApplication
+public class GTAJavaCloudFunction {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTAJavaCloudFunction.class, args);
+	}
+
+	@Bean
+	public Function<String, String> hello() {
+		return value -> "Hello From GTAJava "+value;
+	}
+	@Bean
+	public Function<String, String> bye() {
+		return value -> "Bye From GTAJava "+value;
+	}
+}
---
+++ src/main/java/row/vishal/examples/GTAJavaSingleFunction.java
@@ -0,0 +1,18 @@
+package row.vishal.examples;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+public class GTAJavaSingleFunction {
+	public static void main(String[] args) {
+		SpringApplication.run(GTAJavaSingleFunction.class, args);
+	}
+
+	@Bean
+	public Function<String, String> iAmSingle() {
+		return value -> "ooohooo , I am single function "+value;
+	}
+}
